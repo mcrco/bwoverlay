7a175025429c8c5045d903e4eb79ebe7eb90b631
mod3sty <marco.yang.ca@gmail.com>
2023-09-08 01:39:03 -0700

    fixed log processor to find index of ONLINE

--- src/util/log-processor.ts
+++ src/util/log-processor.ts
@@ -6,7 +6,7 @@ export async function getPlayers(logFilePath: string) {
 
     for (let i = lines.length - 1; i >= 0; i--) {
         if (lines[i].includes('ONLINE: ')) {
-            let players: string[] = lines[i].slice(8).split(', ')
+            let players: string[] = lines[i].slice(lines[i].indexOf('ONLINE') + 8).split(', ')
             return players
         }
     }
